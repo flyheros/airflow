b03c7b27c8238a4d9fe7afa176061112033ba062
jalim <flyheros@naver.com>
2024-12-20 20:38:38 +0900

    d

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="dags_bash_operator",
     schedule="30 20 * * *",
     start_date=pendulum.datetime(2024, 12, 20, tz="Asia/Seoul"),
     catchup=False, # 누락된 일자도 모두 돌릴래? 단, 누락된 일자는 한꺼번에 실행되. 
