cb06efa507a346a002fea9e36fb2b1facb0bf7f4
jalim <flyheros@naver.com>
2024-12-21 13:13:05 +0900

    수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -15,7 +15,7 @@ with DAG(
 ) as dag:
     def select_fruit():
         fruit=['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
-        rand_int = random.randit(0,3)
+        rand_int = random.randint(0,3)
         print(fruit[rand_int])
 
     py_t1=PythonOperator(
