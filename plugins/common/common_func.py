fe7a5fe9694f63ecdf890969837b0075f15dc3f9
jalim <flyheros@naver.com>
2024-12-21 15:13:30 +0900

    op kwargs

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -6,3 +6,12 @@ def regist(name, sex, *args):
     print(f'이름 : {name}')
     print(f'성별 : {sex}')
     print(f'기타타 : {args}')
+
+def regist2(name, sex, *args,  **kwargs):
+    print(f'이름 : {name}')
+    print(f'성별 : {sex}')
+    print(f'기타타 : {args}')
+    email = kwargs['email'] or None
+    address = kwargs['address'] or None
+    print(f'이메일은 {email}')
+    print(f'주소는 {address}')
