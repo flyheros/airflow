b024028e6847920035f4c07de37c5308966ca817
jalim <flyheros@naver.com>
2024-12-21 13:11:18 +0900

    수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -7,7 +7,7 @@ import random
 with DAG(
     dag_id="dags_python_operator",
     schedule="30 20 * * *",
-    start_date=pendulum.datetime(2024, 12, 20, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 12, 21, tz="Asia/Seoul"),
     catchup=False, # 누락된 일자도 모두 돌릴래? 단, 누락된 일자는 한꺼번에 실행되. 
     dagrun_timeout=datetime.timedelta(minutes=60), # timeout 설정정
     tags=["example", "example2"]
