a5116e24105b36c1db91c49126abb7daa3b21b84
jalim <flyheros@naver.com>
2024-12-23 21:11:40 +0900

    dd

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -16,7 +16,7 @@ with DAG(
     # START_DATE : 2주전 월요일, END_DATE : 2주전 토요일
     bash_t2 = BashOperator(
         task_id="bash_t2",  # 화면에 나타나는 것것
-        env= {'START_DATE': '{{ (data_interval_start.in_timezone("Asia/Seoul")  + macros.dateutil.relativedelta.relativedelta(days=-19)) | ds}}',  # 2주전 월요일
+        env= {'START_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul")  + macros.dateutil.relativedelta.relativedelta(days=-19)) | ds}}',  # 2주전 월요일
               'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul")  + macros.dateutil.relativedelta.relativedelta(days=-14)) | ds }}'}, # 2주전 토요일
 
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE : $END_DATE"',
