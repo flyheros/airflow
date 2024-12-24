c7e03ab271d4c5eb73b169c89f3bd83ebba72ca0
jalim <flyheros@naver.com>
2024-12-24 17:19:51 +0900

    dd

--- dags/dags_pyhon_show_templates.py
+++ dags/dags_pyhon_show_templates.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.decorators import task 
 
 with DAG(
-    dag_id="dags_python_show_template",
+    dag_id="dags_python_show_templates",
     schedule="30 20 * * *",
     start_date=pendulum.datetime(2024, 12, 10, tz="Asia/Seoul"),
     catchup=True, # 누락된 일자도 모두 돌릴래? 단, 누락된 일자는 한꺼번에 실행되. 
