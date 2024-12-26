e4ea7057f75078b076170d014142b4a70cd028f6
jalim <flyheros@naver.com>
2024-12-26 22:41:35 +0900

    trigger 추가

---
+++ dags/dags_python_with_trigger_rule_eg1.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+from airflow.task import task
+from airflow.exceptions import AirflowException
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule= None,
+    start_date=pendulum.datetime(2024, 12, 21, tz="Asia/Seoul"),
+    catchup=False, # 누락된 일자도 모두 돌릴래? 단, 누락된 일자는 한꺼번에 실행되. 
+) as dag: 
+    
+    bash_upstream_1 = BashOperator(
+        task_id='task_1',
+        bash_command="echo task1"
+    )
+
+    @task (task_id='task_2')
+    def python_2():
+        raise AirflowException('task_2 Error' )
+
+    
+    @task (task_id='task_3')
+    def python_3():
+       print('task_3 정상 처리 ')
+
+
+
+    @task (task_id='task_4', trigger_rule="all_done")
+    def task_4():
+       print('task_4 정상 처리 ')
+    
+    [bash_upstream_1,  python_2(), python_3()] >> task_4()
---
+++ dags/dags_python_with_trigger_rule_eg2.py
@@ -0,0 +1,54 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from common.common_func import regist2
+
+with DAG(
+    dag_id="dags_python_with_op_kwargs",
+    schedule= None,
+    start_date=pendulum.datetime(2024, 12, 21, tz="Asia/Seoul"),
+    catchup=False, # 누락된 일자도 모두 돌릴래? 단, 누락된 일자는 한꺼번에 실행되. 
+) as dag: 
+    
+    def select_random():
+        import random
+        item_list = ['A','B','C']
+        selected_item = random.choice(item_list)
+        if selected_item=='A':
+            return 'task_a'  ## 다음 실행될 task_id를 적어준다
+        elif selected_item in ['B', 'C']:
+            return ['task_b', 'task_c']  ## 다음 실행될 task_id를 적어준다
+        
+    python_branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
+    )
+
+
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+
+
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func, 
+        op_kwargs={'selected':'A'}
+    )
+
+    
+
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func, 
+        op_kwargs={'selected':'B'}
+    )
+    
+
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func, 
+        op_kwargs={'selected':'C'}
+    )
+
+    python_branch_task >> [task_a, task_b, task_c]
