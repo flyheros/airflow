88484017cfb0710f5eebac28862dab8efab28436
jalim <flyheros@naver.com>
2024-12-26 22:10:13 +0900

    branch decorator

--- dags/dags_branch_python_decorator.py
+++ dags/dags_branch_python_decorator.py
@@ -1,33 +1,29 @@
 from airflow import DAG
 import datetime
 import pendulum
-from airflow.operators.python import BranchPythonOperator, PythonOperator
+from airflow.decorators import task
+from airflow.operators.python import PythonOperator
 
 with DAG(
-    dag_id="dags_branch_python_operator",
+    dag_id="dags_branch_python_decorator",
     schedule="30 20 * * *",
     start_date=pendulum.datetime(2024, 12, 20, tz="Asia/Seoul"),
     catchup=False, # 누락된 일자도 모두 돌릴래? 단, 누락된 일자는 한꺼번에 실행되. 
     # dagrun_timeout=datetime.timedelta(minutes=60), # timeout 설정정
     # tags=["example", "example2"]
-    params={"ti.xcom_push": "ti.xcom_pull"},  # task 에 공통적으로 넘겨줄 변수 
+    # params={"ti.xcom_push": "ti.xcom_pull"},  # task 에 공통적으로 넘겨줄 변수 
 ) as dag:
     
+    @task.branch(task_id='python_branch_task')
     def select_random():
         import random
-        item_list = ['A','B','C']
-        selected_item = random.choice(item_list)
-        if selected_item=='A':
-            return 'task_a'  ## 다음 실행될 task_id를 적어준다
-        elif selected_item in ['B', 'C']:
-            return ['task_b', 'task_c']  ## 다음 실행될 task_id를 적어준다
+        item_ls = ['A', 'B', 'C']
+        selected_item = random.choice(item_ls)
+        if(selected_item=='A'):
+            return 'task_a'
+        elif (selected_item in ['B','C']):
+            return ['task_b', 'task_c']
         
-    python_branch_task = BranchPythonOperator(
-        task_id='python_branch_task',
-        python_callable=select_random
-    )
-
-
     def common_func(**kwargs):
         print(kwargs['selected'])
 
