c69e14ad33a1490e64df82ee86a5b06a720de64e
jalim <flyheros@naver.com>
2025-01-01 21:41:44 +0900

    api

---
+++ dags/dags_api_seoul_corona.py
@@ -0,0 +1,28 @@
+from opnerators.ApiToCsvOperator_Seoul import ApiToCsvOperator_Seoul
+from airflow import DAG
+import pendulum
+
+
+with DAG (
+    dag_id="ApiToCsvOperator_Seoul",
+    schedule="0 7 * * * ",
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    tb_corona19_count_status=ApiToCsvOperator_Seoul(
+        task_id="tb_corona19_count_status",
+        dataset_nm="TbCorona19CountStatus",
+        path="/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone('Asia/Seoul') | ds_nodash }}",
+        file_name="TbCorona19CountStatus_{{data_interval_end.in_timezone('Asia/Seoul') | ds_nodash }}.csv"
+    )
+
+    tv_corona19_vaccine_status=ApiToCsvOperator_Seoul(
+        task_id="tv_corona19_vaccine_status",
+        dataset_nm="TvCorona19VaccineStatus",
+        path="/opt/airflow/files/TvCorona19VaccineStatus/{{data_interval_end.in_timezone('Asia/Seoul') | ds_nodash }}",
+        file_name="TvCorona19VaccineStatus_{{data_interval_end.in_timezone('Asia/Seoul') | ds_nodash }}.csv"
+    )
+
+
+    tb_corona19_count_status >> tv_corona19_vaccine_status
---
+++ plugins/operators/ApiToCsvOperator_Seoul copy.py
@@ -0,0 +1,84 @@
+# from airflow.models.baseoperator import BaseOperator
+# from airflow.hooks.base import BaseHook
+import os
+import pandas as pd
+import requests
+
+
+class ApiToCsvOperator_Seoul():
+    def __init__(self, dataset_nm, path, file_name, base_dt=None):
+        self.http_conn_id = "openapi.seoul.go.kr.http"
+        self.dataset_nm = dataset_nm
+        self.path = path
+        self.file_name = file_name
+        self.endpoint = f"openapi.seoul.go.kr:8088/6f54667168666c793339467774486b/json/{dataset_nm}"
+        self.base_dt = base_dt
+        self.base_url = f"http://{self.endpoint}"
+
+    def execute(self):
+        total_df = pd.DataFrame()
+        start_row = 1
+        add_row= 1000
+        end_row = add_row
+
+        while True:
+            print(f"Fetching data: Start Row = {start_row}, End Row = {end_row}")
+            
+            try:
+                start_row= start_row + add_row
+                end_row = end_row + add_row 
+                base_url = f"{self.base_url}/{start_row}/{end_row}"
+                print(base_url)
+
+                # 데이터 가져오기
+                new_df = self._call_api(base_url, "json", "row", start_row, end_row)
+                total_df = pd.concat([total_df, new_df], ignore_index=True)
+
+                if len(new_df) <add_row :
+                    print(len(new_df),"데이터 끝")
+                    break
+
+            except Exception as e:
+                # print('raise_for_status', response.text)
+                print(f"API 호출 실패: {e}")
+                break
+
+        # 파일 저장
+        self._write_csv(total_df)
+
+    # 실제 API 호출 함수 
+    def _call_api(self, base_url, base_format, key_name, start_row, end_row):
+        response = requests.get(base_url)
+        if response.status_code != 200:
+            raise Exception(f"API 호출 실패: {response.text}")
+        
+        try:
+            if base_format=="json":
+                data=response.json() 
+                df = pd.DataFrame(data.get(self.dataset_nm).get(key_name)) # pd.json_normalize(data['LampScpgmtb']['row'])
+        except Exception as e:
+            print('raise_for_status', response.text)
+            print(f"API 호출 실패: {e}")
+        
+        return df
+    
+    # 파일 저장
+    def _write_csv(self, df):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        
+        if len(df):
+            output_file = os.path.join(self.path, self.file_name)
+            df.to_csv(output_file, index=False, encoding="utf-8-sig")
+            print(f"파일 저장 완료: {output_file}") 
+
+
+# 실행 코드
+if __name__ == "__main__":
+    operator = ApiToCsvOperator_Seoul(
+        dataset_nm="LampScpgmtb",
+        path="./output",
+        file_name="LampScpgmtb.csv"
+    )
+    operator.execute()
+ 
